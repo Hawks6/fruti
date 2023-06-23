9862067b7469842aaaa9b3653db97feaa7eb59a7
Praveen <100776200+Hawks6@users.noreply.github.com>
2023-06-24 04:37:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit as st
 st.title("Save food save life!:moon:")
 st.header("**Hunger meter**")
 x = st.slider('Hunger scale',0,10)  
+file = open('data.txt',"w")
+wt = repr(x)
+file.write(wt)
+file.close()
 st.write("Your hunger rating is",x, 'so you should consume around', x * 50,"calories")
 if st.button('Submit'):
     st.success("successfully submitted")
