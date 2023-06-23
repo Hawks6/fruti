c6a3863768134cc062546a141edf47363f2462fd
Praveen <100776200+Hawks6@users.noreply.github.com>
2023-06-24 03:10:16 +0530

    Update sus.py

--- sus.py
+++ sus.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from deta import Deta
-DETA_KEY = "d0kcaaayith_CVRFVkGkum8n8EUxpQYPXMJfbDdUPYUM"
-deta = Deta(DETA_KEY)
-db = deta.Base("hkth")
+# from deta import Deta
+# DETA_KEY = "d0kcaaayith_CVRFVkGkum8n8EUxpQYPXMJfbDdUPYUM"
+# deta = Deta(DETA_KEY)
+# db = deta.Base("hkth")
 
 def form():
     st.write("**FORM**")
@@ -13,20 +13,20 @@ def form():
         height = st.number_input("Enter your **Height** ! (cm)",0,400)
         choice = st.selectbox('Select your **Gender**',('Male','Female','Other'))
         submission = st.form_submit_button(label = "Submit")
-        insert_data(name,age,weight,height,choice)
-        if submission == True:
-            insert_data(name,age,weight,height,choice)
+        # insert_data(name,age,weight,height,choice)
+        # if submission == True:
+        #     insert_data(name,age,weight,height,choice)
 
     st.success("Successfully submitted")
     st.caption("  [continue](https://fruti-6i5fctpitnw.streamlit.app/) ")
-def insert_data(name,age,weight,height,choice):
-    return db.put({"key":name,"age":age, "wg":weight, "hg":height, "ch":choice})
+# def insert_data(name,age,weight,height,choice):
+#     return db.put({"key":name,"age":age, "wg":weight, "hg":height, "ch":choice})
 
-def fetch_all():
-    res = db.fetch()
-    return res.items
+# def fetch_all():
+#     res = db.fetch()
+#     return res.items
 
-def get_keys(name):
-    return db.get(name)
-fetch_all()
+# def get_keys(name):
+#     return db.get(name)
+# fetch_all()
 form()
