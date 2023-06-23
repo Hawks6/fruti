1066e73e9aeb2122ccfa16ab4e4cebe44cf731b0
Praveen <100776200+Hawks6@users.noreply.github.com>
2023-06-24 04:38:40 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,7 +4,7 @@ f =open('data.txt','r')
 col1, col2, col3 = st.columns(3, gap= "large")
 
 with col1:
-   st.header("Freach Fries")
+   st.header("Freach Fries",f)
    st.image("https://www.awesomecuisine.com/wp-content/uploads/2009/05/french-fries.jpg",width=200)
 
 with col2:
