ca27cf15d9529dc33b6e029b0f4bf3f22e2ccf59
Praveen <100776200+Hawks6@users.noreply.github.com>
2023-06-24 09:14:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,32 @@
 import streamlit as st
+
 st.title("Save food save life!:moon:")
 st.header("**Hunger meter**")
-x = st.slider('Hunger scale',0,10)  
+x = st.slider('Hunger scale',0,5)  
 
-st.write("Your hunger rating is",x, 'so you should consume around', x * 50,"calories")
-if st.button('Submit'):
-    st.success("successfully submitted")
-    st.caption('open [MENU](https://fruti-nca5gzi9h2j.streamlit.app/)')
+
+st.write('we recommend you consume around', x * 200,"calories")
+if(x=200):
+    if st.button('Submit'):
+        st.success("successfully submitted")
+        st.caption('open [MENU]()')
+
+if(x=400):
+    if st.button('Submit'):
+        st.success("successfully submitted")
+        st.caption('open [MENU]()')
+
+if(x=600):
+    if st.button('Submit'):
+        st.success("successfully submitted")
+        st.caption('open [MENU]()')
+
+if(x=800):
+    if st.button('Submit'):
+        st.success("successfully submitted")
+        st.caption('open [MENU]()')
+
+if(x=1000):
+    if st.button('Submit'):
+        st.success("successfully submitted")
+        st.caption('open [MENU]()')
