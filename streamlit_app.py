adafcff454ed962643514f716d085aeb9827e375
Praveen <100776200+Hawks6@users.noreply.github.com>
2023-06-24 14:35:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title("Save food save life!:moon:")
-st.header("**Hunger meter**")
-x = st.slider('**How hungry are you?**',1,5)  
+st.header("**How hungry are you?**")
+x = st.slider(1,5)  
 
 
 st.write('we recommend you consume around', x * 200,"calories")
