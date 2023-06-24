e71cb45dc5adc3df0c1e808450e27aae8d4b2f7b
Praveen <100776200+Hawks6@users.noreply.github.com>
2023-06-24 16:01:24 +0530

    Update Menu10000.py

--- Menu10000.py
+++ Menu10000.py
@@ -14,21 +14,25 @@ with col1:
       st.success('Best Before : 8 Hours, Store in a cool, dry place.')
    st.text_input(label="Leave a comment", key="ab");
 with col2:
-   st.subheader("French Fries")
-   st.caption("200 calories")
-   st.image("https://www.awesomecuisine.com/wp-content/uploads/2009/05/french-fries.jpg",width=200)
-   st.number_input(label="Quantity", step=1, min_value=0, key="a")
-   if st.button('Add', key="j"):
-      st.success('Best Before : 2 Hours, Consume Immediately')
-   st.text_input(label="Leave a comment", key="bb");
+   st.subheader("Chole Bhature")
+   st.caption("900 calories")
+   st.image("https://media.vogue.in/wp-content/uploads/2020/08/chole-bhature-recipe.jpg",width=200)
+   st.number_input(label="Quantity", step=1, min_value=0, key="h")
+   if st.button('Add', key="q"):
+      st.success('Best Before : 4 Hours, To be Consumed Piping Hot and Fresh')
+   st.text_input(label="Leave a comment", key="bk");
+
+  
 with col3:
-   st.subheader("Omlette")
-   st.caption("300 calories")
-   st.image("https://www.mygorgeousrecipes.com/wp-content/uploads/2018/02/Worlds-Best-Vegetarian-Omelette-Quick-and-Easy.jpg",width=200)
-   st.number_input(label="Quantity", step=1, min_value=0, key="b")
-   if st.button('Add', key="k" ):
-      st.success('Best Before : 4 Hours, Refridgerate and Reheat if consuming later')
-   st.text_input(label="Leave a comment",key="bc");
+   st.subheader("Mac n Cheese")
+   st.caption("800 calories")
+   st.image("https://www.allrecipes.com/thmb/X94tJBe-nYseXIbMXo03pVXWnRA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/238691-Simple-Macaroni-And-Cheese-mfs_008-7a50b284b67140919a0984093cb4611b.jpg",width=200)
+   st.number_input(label="Quantity", step=1, min_value=0, key="g")
+   if st.button('Add', key="p"):
+      st.success('Best Before : 3 Hours, Store in an Air-Tight Container and Reheat if consuming later')
+   st.text_input(label="Leave a comment", key="bj");
+   
+  
 st.divider()
 
 col4, col5, col6 = st.columns(3,gap= "large")
@@ -44,13 +48,14 @@ with col4:
 
 
 with col5:
-   st.subheader("Chicken kebab")
-   st.caption("500 calories")
-   st.image("https://www.thetakeiteasychef.com/wp-content/uploads/2016/12/unpaid-review-easterns-kebab-masala.1024x1024-3.png",width=200)
-   st.number_input(label="Quantity", step=1, min_value=0, key="d")
-   if st.button('Add', key="m"):
-      st.success('Best Before : 4 Hours, Refridgerate and Reheat until steaming hot if consuming later')
-   st.text_input(label="Leave a comment", key="be");
+    st.subheader("Pepperoni Pizza")
+   st.caption("700 calories")
+   st.image("https://www.simplyrecipes.com/thmb/I4razizFmeF8ua2jwuD0Pq4XpP8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg",width=200)
+   st.number_input(label="Quantity", step=1, min_value=0, key="f")
+   if st.button('Add', key="o"):
+      st.success('Best Before : 4 Hours, Refridgerate below 40 Farenheit and Reheat if consuming later')
+   st.text_input(label="Leave a comment", key="bh");
+  
 
 with col6:
    st.subheader("Chicken Burger Meal")
@@ -64,30 +69,32 @@ with col6:
 col7, col8, col9 = st.columns(3,gap= "large")
 
 with col7:
-   st.subheader("Pepperoni Pizza")
-   st.caption("700 calories")
-   st.image("https://www.simplyrecipes.com/thmb/I4razizFmeF8ua2jwuD0Pq4XpP8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg",width=200)
-   st.number_input(label="Quantity", step=1, min_value=0, key="f")
-   if st.button('Add', key="o"):
-      st.success('Best Before : 4 Hours, Refridgerate below 40 Farenheit and Reheat if consuming later')
-   st.text_input(label="Leave a comment", key="bh");
+   st.subheader("Chicken kebab")
+   st.caption("500 calories")
+   st.image("https://www.thetakeiteasychef.com/wp-content/uploads/2016/12/unpaid-review-easterns-kebab-masala.1024x1024-3.png",width=200)
+   st.number_input(label="Quantity", step=1, min_value=0, key="d")
+   if st.button('Add', key="m"):
+      st.success('Best Before : 4 Hours, Refridgerate and Reheat until steaming hot if consuming later')
+   st.text_input(label="Leave a comment", key="be");
+  
 
 
 with col8:
-   st.subheader("Mac n Cheese")
-   st.caption("800 calories")
-   st.image("https://www.allrecipes.com/thmb/X94tJBe-nYseXIbMXo03pVXWnRA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/238691-Simple-Macaroni-And-Cheese-mfs_008-7a50b284b67140919a0984093cb4611b.jpg",width=200)
-   st.number_input(label="Quantity", step=1, min_value=0, key="g")
-   if st.button('Add', key="p"):
-      st.success('Best Before : 3 Hours, Store in an Air-Tight Container and Reheat if consuming later')
-   st.text_input(label="Leave a comment", key="bj");
+   st.subheader("Omlette")
+   st.caption("300 calories")
+   st.image("https://www.mygorgeousrecipes.com/wp-content/uploads/2018/02/Worlds-Best-Vegetarian-Omelette-Quick-and-Easy.jpg",width=200)
+   st.number_input(label="Quantity", step=1, min_value=0, key="b")
+   if st.button('Add', key="k" ):
+      st.success('Best Before : 4 Hours, Refridgerate and Reheat if consuming later')
+   st.text_input(label="Leave a comment",key="bc");
+   
 
 with col9:
-   st.subheader("Chole Bhature")
-   st.caption("900 calories")
-   st.image("https://media.vogue.in/wp-content/uploads/2020/08/chole-bhature-recipe.jpg",width=200)
-   st.number_input(label="Quantity", step=1, min_value=0, key="h")
-   if st.button('Add', key="q"):
-      st.success('Best Before : 4 Hours, To be Consumed Piping Hot and Fresh')
-   st.text_input(label="Leave a comment", key="bk");
+   st.subheader("French Fries")
+   st.caption("200 calories")
+   st.image("https://www.awesomecuisine.com/wp-content/uploads/2009/05/french-fries.jpg",width=200)
+   st.number_input(label="Quantity", step=1, min_value=0, key="a")
+   if st.button('Add', key="j"):
+      st.success('Best Before : 2 Hours, Consume Immediately')
+   st.text_input(label="Leave a comment", key="bb");
 
