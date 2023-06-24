20dbcb5f02a2d9ebd4b41a4327ec06fb54431fd4
Praveen <100776200+Hawks6@users.noreply.github.com>
2023-06-24 09:23:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,31 +2,31 @@ import streamlit as st
 
 st.title("Save food save life!:moon:")
 st.header("**Hunger meter**")
-x = st.slider('Hunger scale',0,5)  
+x = st.slider('Hunger scale',1,5)  
 
 
 st.write('we recommend you consume around', x * 200,"calories")
-if(x==0):
+if(x==1):
     if st.button('Submit'):
         st.success("successfully submitted")
-        st.caption('open [MENU]()')
+        st.caption('open [MENU](https://fruti-jym0qgj9ccs.streamlit.app/)')
 
 if(x==2):
     if st.button('Submit'):
         st.success("successfully submitted")
-        st.caption('open [MENU]()')
+        st.caption('open [MENU](https://fruti-3luk32bl952.streamlit.app/)')
+
+if(x==3):
+    if st.button('Submit'):
+        st.success("successfully submitted")
+        st.caption('open [MENU](https://fruti-rmbzzumxvep.streamlit.app/)')
 
 if(x==4):
     if st.button('Submit'):
         st.success("successfully submitted")
-        st.caption('open [MENU]()')
+        st.caption('open [MENU](https://fruti-6d9zuuf01bo.streamlit.app/)')
 
-if(x==6):
+if(x==5):
     if st.button('Submit'):
         st.success("successfully submitted")
-        st.caption('open [MENU]()')
-
-if(x==8):
-    if st.button('Submit'):
-        st.success("successfully submitted")
-        st.caption('open [MENU]()')
+        st.caption('open [MENU](https://fruti-aqh0ud8om0l.streamlit.app/)')
