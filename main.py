46c1984821c35edeee8523d7c673f5f8f5298b69
Praveen <100776200+Hawks6@users.noreply.github.com>
2023-06-24 15:45:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ header_left, header_mid, header_right = st.columns([1,3,1],gap = 'large')
 
 with header_mid:
     st.title('Hello!')
-    st.header('**Welcome to MANNA !!**:')
+    st.header('**Welcome to Manna !!**')
     st.header('Choose the Service you would like to Avail :')
     st.subheader('Are You a [Customer](https://fruti-tlso26160en.streamlit.app/) or a [Business](https://fruti-0ggql26iuzi.streamlit.app/)?')
 
