af07d1a144c813bc9ecb78b87b99201af5a4ee54
Praveen <100776200+Hawks6@users.noreply.github.com>
2023-06-24 06:33:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
 import streamlit as st
+from deta import Deta
 st.title("Save food save life!:moon:")
 st.header("**Hunger meter**")
 x = st.slider('Hunger scale',0,10)  
-file = open('data.txt',"w")
-wt = repr(x)
-file.write(wt)
-file.close()
+deta = Deta(st.secrets["d0e7kgqnw4j_diMQAoXmbyn4iWV7fF1cQRVrXvZDwypq"])
+db = deta.Base("example-db")
+
 st.write("Your hunger rating is",x, 'so you should consume around', x * 50,"calories")
 if st.button('Submit'):
+    if submitted:
+        db.put({"value" : x})
     st.success("successfully submitted")
     st.caption('open [MENU](https://fruti-nca5gzi9h2j.streamlit.app/)')
+db_content = db.fetch().items
+st.write(db_content)
